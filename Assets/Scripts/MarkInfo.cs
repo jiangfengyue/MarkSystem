6f79bc46641887c6ee3f3ac34677a9b275ac9e91
jiangfengyue <jamesclarke@qq.com>
2017-01-04 22:31:41 +0800

    根据视点远近调整标注的大小和颜色

--- Assets/Scripts/MarkInfo.cs
+++ Assets/Scripts/MarkInfo.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 // 21/12/2016 - TAG: Just a test
@@ -7,26 +8,27 @@ public class MarkInfo : MonoBehaviour {
 	public Vector3 m_fSatPos;
 	public Camera m_MainCam;
 
+	Image m_Image;
 	public float m_fOrder;
 	// Use this for initialization
 	void Start () {
 		if(m_MainCam==null)
 			m_MainCam = GameObject.Find("Main Camera").GetComponent<Camera>();
-
+		m_Image = transform.GetComponent<Image>();
 //		m_fSatPos = new Vector3(m_MainCam.GetComponent<CamRotate>().m_fMinDistance, m_MainCam.GetComponent<CamRotate>().m_fMinDistance, m_MainCam.GetComponent<CamRotate>().m_fMinDistance);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-/*		if(Vector3.AngleBetween(m_fSatPos,m_MainCam.transform.position)>90)
-		{
-			transform.position = new Vector3(0,0,-1000);
-			return;
-		}
+
 		transform.position = m_MainCam.WorldToScreenPoint(m_fSatPos);
 		m_fOrder = transform.position.z;
+		float fScale = Mathf.Clamp(10000/m_fOrder,0.1F,1F);
+//		m_Image.color = new Color(m_Image.color.r, m_Image.color.g, m_Image.color.b, fScale);
+
 		transform.position=new Vector3(transform.position.x, transform.position.y, 0);
-*/
-		transform.position = m_fSatPos;
+		transform.localScale = Vector3.one * fScale;
+
+//		transform.position = m_fSatPos;
 	}
 }
