35c6312b923331c0b6ae3f2c62dc19729edf0d98
jiangfengyue <jamesclarke@qq.com>
2017-01-04 08:57:22 +0800

    修改了摄像机的操控代码，利用三角函数将鼠标位移转换为空间坐标，但是不知道为什么上下视点偏角总是不能到达最大。

--- Assets/Scripts/CamRotate.cs
+++ Assets/Scripts/CamRotate.cs
@@ -1,77 +1,103 @@
 ﻿using UnityEngine;
-using System.Collections;
-using UnityStandardAssets.CrossPlatformInput;
-public class CamRotate : MonoBehaviour {
+public class CamRotate:MonoBehaviour {
 
-	[SerializeField] protected Transform m_Target;            // The target object to follow
-    [SerializeField] private float m_MoveSpeed = 1f;                      // How fast the rig will move to keep up with the target's position.
+	[SerializeField] protected Transform m_Target; // The target object to follow
     [Range(0f, 10f)] [SerializeField] private float m_TurnSpeed = 1.5f;   // How fast the rig will rotate from user input.
     [SerializeField] private float m_TurnSmoothing = 0.0f;                // How much smoothing to apply to the turn input, to reduce mouse-turn jerkiness
-    [SerializeField] private float m_TiltMax = 75f;                       // The maximum value of the x axis rotation of the pivot.
-    [SerializeField] private float m_TiltMin = 45f;                       // The minimum value of the x axis rotation of the pivot.
 
 	private float m_LookAngle;                    // The rig's y axis rotation.
-    private float m_TiltAngle;                    // The pivot's x axis rotation.
+	private float m_TiltAngle; // The pivot's x axis rotation.
     private float k_LookDistance = 100f;    // How far in front of the pivot the character's look target is.
-	private Vector3 m_PivotEulers;
-	private Vector3 m_vDir;
+	private Vector3 m_vDir; 
 
-	public float m_fMinDistance;		//与物体的最短距离，为物体半径的1.5倍
+	public float m_fMinDistance; 		//与物体的最短距离，为物体半径的1.5倍
 
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
-    protected void Awake()
-    {
-		m_vDir = transform.position - m_Target.position;
-		k_LookDistance = m_vDir.magnitude;
-		transform.LookAt(m_Target);  		     
-		m_PivotEulers = Quaternion.Inverse(transform.rotation).eulerAngles ;
-		MeshFilter mf = m_Target.GetComponent<MeshFilter>();
-		m_fMinDistance = mf.mesh.bounds.size.x*1.5f/2f;
-    }
+    protected void Awake() {
+		m_vDir = transform.position - m_Target.position; 
+		k_LookDistance = m_vDir.magnitude; 
+		transform.LookAt(m_Target); 	
+		m_TiltAngle = 90 - Vector3.Angle(m_vDir, Vector3.up);
+		m_LookAngle = Vector3.Angle(Vector3.Cross(Vector3.up, Vector3.Cross(m_vDir, Vector3.up)) ,Vector3.forward);
+		MeshFilter mf = m_Target.GetComponent < MeshFilter > (); 
+		m_fMinDistance = mf.mesh.bounds.size.x * 1.5f/2f; 
+}
 
 	// Update is called once per frame
 	void Update () {
-		HandleCamera();
+		HandleCamera(); 
 	}
 
-	private void HandleCamera()
-	{
-		if(Time.timeScale < float.Epsilon)
-			return;
+	private void HandleCamera() {
+		if (Time.timeScale < float.Epsilon)
+			return; 
 
-		var z = Input.GetAxis("Mouse ScrollWheel")/20+1;
+		var z = Input.GetAxis("Mouse ScrollWheel")/20 + 1; 
 
-		m_vDir *= z;
+		m_vDir *= z; 
 
-		if(Input.GetMouseButton(1))
-		{
-			// Read the user input
-			var x = Input.GetAxis("Mouse X")*m_TurnSpeed;
-			var y = Input.GetAxis("Mouse Y")*m_TurnSpeed;
-
-
-			// Adjust the look angle by an amount proportional to the turn speed and horizontal input.
-			m_LookAngle = m_PivotEulers.y + 0;
-			// on platforms with a mouse, we adjust the current angle based on Y mouse input and turn speed
-			m_TiltAngle = m_PivotEulers.x - 0;
-			// and make sure the new value is within the tilt range
-			m_TiltAngle = Mathf.Clamp(m_TiltAngle, -m_TiltMin, m_TiltMax);
-
-
-			m_vDir = Quaternion.Euler(y, x, 0) * m_vDir;
-			m_vDir = m_vDir.normalized * m_vDir.magnitude;
+		if (Input.GetMouseButton(1)) {
+			Rotate(); 
 		}
 
 		if (m_TurnSmoothing > 0)
-			transform.position = Vector3.Lerp(transform.position, m_vDir, m_TurnSmoothing * Time.deltaTime);
+			transform.position = Vector3.Lerp(transform.position, m_vDir, m_TurnSmoothing * Time.deltaTime); 
 		else
-			transform.position = m_vDir;
+			transform.position = m_vDir; 
 
-		transform.LookAt(m_Target);	
+		transform.LookAt(m_Target); 	
+		m_vDir = transform.position;
 		
+	}
+
+	private void Rotate_Euler() {
+			// Read the user input
+			var x = Input.GetAxis("Mouse X") * m_TurnSpeed; 
+			var y = Input.GetAxis("Mouse Y") * m_TurnSpeed; 
+
+
+			m_vDir = Quaternion.Euler(y, x, 0) * m_vDir; 
+			m_vDir = m_vDir.normalized * m_vDir.magnitude; 
+	}
+
+	private void Rotate_Quaternion() {
+			// Read the user input
+			var x = Input.GetAxis("Mouse X") * m_TurnSpeed; 
+			var y = Input.GetAxis("Mouse Y") * m_TurnSpeed; 	
+			float fDis = m_vDir.magnitude; 
+
+			Quaternion q1 = Quaternion.AngleAxis(x, Vector3.up); 
+			
+			m_vDir = Quaternion.AngleAxis(y, transform.right) * m_vDir;
+
+			m_vDir = q1 * m_vDir; 
+			m_vDir = m_vDir.normalized * fDis; 			
+	}
+
+	//利用三角函数计算空间位置
+	private void Rotate() {
+			var x = Input.GetAxis("Mouse X") * m_TurnSpeed; 
+			var y = Input.GetAxis("Mouse Y") * m_TurnSpeed; 
+
+			m_LookAngle += x;
+			m_TiltAngle += y;
+
+			float fArcTilt = Angle2Arc(m_TiltAngle);
+			float fArcLook = Angle2Arc(m_LookAngle);
+			float fPosY = Mathf.Sin(fArcTilt);
+			float fPosXZ = Mathf.Cos(fArcTilt);
+			float fPosZ = Mathf.Cos(fArcLook);
+			float fPosX = Mathf.Sin(fArcLook);
+
+			m_vDir = new Vector3(fPosX, fPosY, fPosZ).normalized * m_vDir.magnitude;
+	}
+
+	float Angle2Arc(float _fAngle)
+	{
+		return _fAngle*Mathf.PI/180;
 	}
 }
